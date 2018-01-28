ac0e4fab133a0125d846cd511233d51f9813d539
srianton <Stevanus.Riantono@amd.com>
2018-01-27 23:33:24 -0600

    Formatting Messages Display

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,10 +1,31 @@
 import React from 'react';
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 const Message = ({ incoming, message }) => (
-    <View>
+    <View style={[
+        styles.listItem, incoming ?
+            styles.incomingMessage :
+            styles.outgoingMessage
+    ]}>
         <Text>{message}</Text>
     </View>
 )
+
+const styles = StyleSheet.create({
+    listItem: {
+        width: '75%',
+        margin: 5,
+        padding: 5,
+        backgroundColor: 'white',
+        borderColor: '#979797',
+        borderStyle: 'solid',
+        borderWidth: 1,
+        borderRadius: 10
+    },
+    incomingMessage: {
+        alignSelf: 'flex-end',
+        backgroundColor: '#E1FFC7'
+    } 
+})
 
 export default Message;
